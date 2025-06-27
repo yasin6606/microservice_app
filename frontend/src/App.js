a322e036c15716cd6494f123808daf7312cafe90
yasin <yasingoorakani@gmail.com>
2025-06-28 00:35:21 +0330

    add more styles to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 // src/App.js
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import DataDisplay from './DataDisplay';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import DataDisplay from './Pages/DataDisplay';
+// import "@tailwindcss"
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(true);
 
-  return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/data" element={isAuthenticated ? <DataDisplay /> : <Navigate to="/login" />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <div className="h-screen">
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated}/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/data" element={isAuthenticated ? <DataDisplay/> : <Navigate to="/login"/>}/>
+                    <Route path="/" element={<Navigate to="/login"/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
